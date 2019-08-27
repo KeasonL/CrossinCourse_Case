f5994542399f3cd087d884bc0aab4e7e6fe64c6d
KeasonL <37066807+KeasonL@users.noreply.github.com>
2019-08-27 12:04:29 +0800

    Create course2_var.py

---
+++ course2_var.py
@@ -0,0 +1,13 @@
+# 2. 各种变量
+
+a = 'hello,world'
+b = 1.414
+c = (1 != 2)
+d = (True and False)
+e = (True or False)
+print ("a:%s,b:%.2f,c:%s,d:%s,e:%s" %(a,b,c,d,e))
+
+f = '''
+'hello,world'\n\\"hello,world"
+'''
+print(f)
